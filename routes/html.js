cde7d3fd5fa5aa8962978c606eddba76f7d13c32
Cindy Burton <cindy.brton@gmail.com>
2020-07-06 18:27:08 -0600

    working on routes for heroku deployment

--- routes/html.js
+++ routes/html.js
@@ -1,12 +1,12 @@
 const router = require('express').Router()
 const path = require('path')
 
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
+router.get(['/', ''], (req, res) => {
+    res.sendfile(path.join(__dirname, '../public/index.html'))
 })
 
 router.get('/:month/budget/:id', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/budget.html'))
+    res.sendfile(path.join(__dirname, '../public/budget.html'))
 })
 
 module.exports = router
